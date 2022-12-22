29e81f01bc0589b7ae18e2f063d372bf573f4e7c
wardaelsayed77 <121184297+wardaelsayed77@users.noreply.github.com>
2022-12-22 20:45:18 +0200

    Add files via upload

---
+++ Perfect_number/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum;
+            int n1, n2;
+            Console.WriteLine("Find perfect numbers within a given number of range:\n");
+            Console.WriteLine("enter the start number : ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the end number : ");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("The Perfect numbers within the given range : ");
+            for (int x = n1; x <= n2; x++)
+            {
+                sum = 0;
+                for (int y = 1; y < x; y++)
+                {
+                    if (x % y == 0)
+                        sum += y;
+                }
+                if (sum == x)
+                    Console.WriteLine("{0} ", x);
+            }
+            Console.WriteLine();
+        }
+    }
+}
